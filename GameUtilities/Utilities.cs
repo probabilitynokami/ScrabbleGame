06f606fc9ac984043db40942c647722da4683dae
Dion Solang <gumball3point@gmail.com>
2024-03-13 08:15:35 +0700

    feat: added utility stuff

---
+++ GameUtilities/Utilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Immutable;
+
+namespace GameUtilities;
+
+
+public static class Extensions{
+    public static T Shuffle<T>(this T collections) where T : IList{
+        // TODO: do real shuffle here
+        return collections;
+    }
+}

b5fb26633504f909e996342adc43e2bd18864ab2
Dion Solang <gumball3point@gmail.com>
2024-03-13 11:26:04 +0700

    feat(utility): add aho-corasick string pattern matching

--- GameUtilities/Utilities.cs
+++ GameUtilities/Utilities.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 
 namespace GameUtilities;
 
@@ -7,5 +7,77 @@ public static class Extensions{
     public static T Shuffle<T>(this T collections) where T : IList{
         // TODO: do real shuffle here
         return collections;
+    }
+}
+
+
+public class AhoCorasickTrie{
+    private AhoCorasickNode root;
+
+    public AhoCorasickTrie(){
+        root = new AhoCorasickNode('.',false);
+    }
+    public AhoCorasickTrie(IEnumerable<string> wordList) : this(){
+        // build wordlist here
+        foreach(var word in wordList){
+            RegisterWord(word);
+        }
+    }
+
+    public void RegisterWord(string word){
+        var currentNode = root;
+        foreach(var c in word){
+            var nextNode = currentNode.nextNode(c);
+            if (nextNode is null){
+                var newNode = new AhoCorasickNode(c,false);
+                currentNode.RegisterNode(newNode);
+                currentNode = newNode;
+            }
+            else{
+                currentNode = nextNode;
+            }
+        }
+        currentNode.Stopable = true;
+    }
+
+    public bool CheckWord(string word){
+        var currentNode = root;
+        foreach(var c in word){
+            var nextNode = currentNode.nextNode(c);
+            if (nextNode is null){
+                return false;
+            }
+            else{
+                currentNode = nextNode;
+            }
+        }
+        return currentNode.Stopable;
+    }
+}
+
+internal class AhoCorasickNode{
+    public char Value{get; set;}
+    public bool Stopable{get; set;}
+
+    public AhoCorasickNode(char value, bool stopable){
+        Value = value;
+        Stopable = stopable;
+        transitionTable = [];
+    }
+    private Dictionary<char,AhoCorasickNode> transitionTable;
+
+    internal AhoCorasickNode? nextNode(char edge){
+        if (!transitionTable.TryGetValue(edge, out AhoCorasickNode? value))
+            return null;
+        return value;
+    }
+
+    internal void RegisterNode(AhoCorasickNode node){
+        var edge = node.Value;
+
+        if(transitionTable.ContainsKey(edge))
+            return;
+
+        transitionTable.Add(edge, node);
     }
 }

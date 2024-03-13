e06880f8796a543e36c810ac160609fdb0dc28eb
Dion Solang <gumball3point@gmail.com>
2024-03-13 13:27:36 +0700

    feat: remove playerdata from player, will use external dict instead

--- GameObjects/Player.cs
+++ GameObjects/Player.cs
@@ -6,11 +6,9 @@ public class ScrabblePlayer : IPlayer
     public int ID{get;}
     private string _name = "";
     public string Name { get => _name; set {_name = value;} }
-    public PlayerData data;
 
     public ScrabblePlayer(){
         ID = 0;
         _name = "";
-        data = new PlayerData();
     }
 }

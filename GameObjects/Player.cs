fe78138b79ca5ef2544fd37c493249c8bf0c1a94
Dion Solang <gumball3point@gmail.com>
2024-03-13 13:26:36 +0700

    feat: add playerdata to player

--- GameObjects/Player.cs
+++ GameObjects/Player.cs
@@ -1,9 +1,16 @@
 using GameObjects;
 
 
-class ScrabblePlayer : IPlayer
+public class ScrabblePlayer : IPlayer
 {
     public int ID{get;}
     private string _name = "";
     public string Name { get => _name; set {_name = value;} }
+    public PlayerData data;
+
+    public ScrabblePlayer(){
+        ID = 0;
+        _name = "";
+        data = new PlayerData();
+    }
 }

584ba0739803c8f2be1d0d6687f5d6b7bee28803
Dion Solang <gumball3point@gmail.com>
2024-03-19 00:53:35 +0700

    feat: init TUI

---
+++ SafeGameController/SafeGameControl.cs
@@ -0,0 +1,88 @@
+﻿using GameController;
+using GameObjects;
+using GameUtilities;
+namespace SafeGameController;
+
+public interface IGUIBackendInterface{
+    public PlayerData GetCurrentPlayerData();
+    public IBoard GetCurrentBoard();
+    public List<ISquare> GetPlacedSquares();
+    
+
+    // commands
+    public bool SelectTileOnRack(int index);
+    public bool PlaceTileOnBoard(BoardPosition position);
+    public bool SkipTurn();
+    public bool NextTurn();
+    public bool ShuffleDeck();
+}
+public class SafeGameController : IGUIBackendInterface
+{
+    private GameControl gameController;
+    
+    private ITile? tileOnHand = null;
+    
+    public SafeGameController(IGamePopulator populator){
+        gameController = new(populator);
+    }
+
+    public IBoard GetCurrentBoard()
+    {
+        return gameController.GetBoard();
+ 
+    }
+
+    public PlayerData GetCurrentPlayerData()
+    {
+        return gameController.GetCurrentPlayerData();
+    }
+
+    public List<ISquare> GetPlacedSquares()
+    {
+        return gameController.gameState.placedSquares;
+    }
+
+    public bool NextTurn()
+    {
+        if(!gameController.gameState.IsSquarePositionsValid())
+            return false;
+        gameController.NextTurn();
+        return true;
+    }
+
+    public bool PlaceTileOnBoard(BoardPosition position)
+    {
+        if(tileOnHand is null)
+            return false;
+        if(gameController.GetBoard().Squares[position.row,position.column].Occupied)
+            return false;
+
+        gameController.PlaceTile(tileOnHand,position);
+
+        return true;
+    }
+
+    public bool SelectTileOnRack(int index)
+    {
+        var rack = gameController.GetCurrentPlayerData().Rack;
+        if(!(0 <= index && index <= rack.RackSize))
+            return false;
+        if(tileOnHand is not null)
+            return false;
+        tileOnHand = rack.TakeTile(index);
+
+        return tileOnHand is not null;
+    }
+
+    public bool ShuffleDeck()
+    {
+        // TODO: implement this
+        throw new NotImplementedException();
+    }
+
+    public bool SkipTurn()
+    {
+        // TODO: implement this
+        throw new NotImplementedException();
+    }
+}
---
+++ TextUserInterface/TextUserInterface.cs
@@ -0,0 +1,27 @@
+﻿using GameUtilities;
+namespace TextUserInterface;
+
+public class TextUserInterface
+{
+    private string[,] content;
+    private Func<bool>[,] cellFunc;
+    
+    private int cursorRow;
+    private int cursorColumn;
+    public void HandleInput(char input){
+        switch(input){
+            case 'h': cursorRow = Math.Max(cursorRow-1,0); break;
+            case 'l': cursorRow = Math.Min(cursorRow+1,0); break;
+            case 'j': cursorColumn = Math.Min(cursorColumn+1,0); break;
+            case 'k': cursorColumn = Math.Max(cursorColumn-1,0); break;
+            case ' ': cellFunc[cursorRow,cursorColumn].Invoke(); break;
+        }
+    }
+    
+    public TextUserInterface(Size size){
+        content = new string[size.Height, size.Width];
+        content.Initialize();
+        cellFunc = new Func<bool>[size.Height, size.Width];
+        content.Initialize();
+    }
+}

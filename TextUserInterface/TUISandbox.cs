5ce851ebfa6c0423e6b74dc3a08dd17f409b5f83
Dion Solang <gumball3point@gmail.com>
2024-03-19 09:22:48 +0700

    feat: added working cursor and grid

--- TextUserInterface/TUISandbox.cs
+++ TextUserInterface/TUISandbox.cs
@@ -4,11 +4,24 @@ using TextUserInterface;
 
 
 public class TUISandbox {
-    static void Main(){
+    static async Task Main(){
         var tui = new TUI(new GameUtilities.Size(5,5));
-        while(true){
-            tui.Render();
-        }
+        var inputTask = Task.Run(() => {
+            while(true)
+                tui.HandleInput(Console.ReadKey(true).KeyChar);
+        });
+
+        var renderTask = Task.Run(async () => {
+            while(true){
+                tui.UpdateContent();
+                tui.UpdateBuffer();
+                tui.Render();
+                await Task.Delay(100);
+            }
+        });
+
+        await inputTask;
+        await renderTask;
     }
 
 }
--- TextUserInterface/TextUserInterface.cs
+++ TextUserInterface/TextUserInterface.cs
@@ -5,28 +5,45 @@ namespace TextUserInterface;
 public class TUI
 {
     private string[][] content;
+    private bool[][] highlight;
     private Action[][] cellFunc;
+
+    private string[][] renderBuffer;
+
+    private Text textBuffer;
 
     private readonly GameUtilities.Size TUISize;
     private Grid spectreGrid;
+
+    public bool RerenderFlag{get; set;} = true;
     
     private int cursorRow;
     private int cursorColumn;
-    public void HandleInput(char input){
+    public async Task HandleInput(char input){
         switch(input){
-            case 'h': cursorRow = Math.Max(cursorRow-1,0); break;
-            case 'l': cursorRow = Math.Min(cursorRow+1,TUISize.Height); break;
-            case 'j': cursorColumn = Math.Min(cursorColumn+1,TUISize.Width); break;
-            case 'k': cursorColumn = Math.Max(cursorColumn-1,0); break;
-            case ' ': cellFunc[cursorRow][cursorColumn].Invoke(); break;
+            case 'k': cursorRow = Math.Max(cursorRow-1,0); break;
+            case 'j': cursorRow = Math.Min(cursorRow+1,TUISize.Height-1); break;
+            case 'l': cursorColumn = Math.Min(cursorColumn+1,TUISize.Width-1); break;
+            case 'h': cursorColumn = Math.Max(cursorColumn-1,0); break;
+            case ' ': await Task.Run(() => cellFunc[cursorRow][cursorColumn].Invoke()); break;
         }
+        RerenderFlag = true;
     }
     
     public TUI(GameUtilities.Size size){
         TUISize = size;
 
         content = new string[size.Height][];
-        content.Initialize("X",size.Width);
+        content.Initialize("H",size.Width);
+
+        renderBuffer = new string[size.Height][];
+        renderBuffer.Initialize(" ",size.Width);
+
+        highlight = new bool[size.Height][];
+        highlight.Initialize(false,size.Width);
+
+        textBuffer = new Text("");
+
 
         cellFunc = new Action[size.Height][];
         cellFunc.Initialize(()=>{}, size.Width);
@@ -35,19 +52,57 @@ public class TUI
         spectreGrid.AddColumns(size.Width);
         for(int i=0;i<size.Height;i++)
             spectreGrid.AddRow(content[i]);
+
+        for(int i=0;i<size.Height;i++){
+            for(int j=0;j<size.Width;j++){
+                int storeI=i, storeJ=j;
+                cellFunc[i][j] = () => {
+                    if(content[storeI][storeJ] == "H")
+                        content[storeI][storeJ] = "X";
+                    else
+                        content[storeI][storeJ] = "H";
+                };
+            }
+        }
     }
 
-    public void UpdateBuffer(){
-        spectreGrid = new Grid();
-        for(int i=0;i<TUISize.Height;i++)
-            for(int j=0;j<TUISize.Width;j++)
-                content[i][j] = "H";
+    public void UpdateContent(){
+        for(int i=0;i<TUISize.Height;i++){
+            for(int j=0;j<TUISize.Width;j++){
+                if(i==cursorRow && j==cursorColumn){
+                    highlight[i][j] = true;
+                }
+                else{
+                    highlight[i][j] = false;
+                }
+            }
+        }
 
-        for(int i=0;i<TUISize.Height;i++)
-            spectreGrid.AddRow(content[i]);
+    }
+    public void UpdateBuffer(){
+        for(int i =0;i<TUISize.Height;i++){
+            for(int j=0;j<TUISize.Width;j++){
+                if(highlight[i][j]){
+                    renderBuffer[i][j] = $"[underline green]{content[i][j]}[/] ";
+                }
+                else{
+                    renderBuffer[i][j] = $"[red]{content[i][j]}[/] ";
+                }
+            }
+        }
     }
 
     public void Render(){
-        AnsiConsole.Write(spectreGrid);
+        if(!RerenderFlag){
+            return;
+        }
+        AnsiConsole.Clear();
+        for(int i =0;i<TUISize.Height;i++){
+            for(int j=0;j<TUISize.Width;j++){
+                AnsiConsole.Markup(renderBuffer[i][j]);
+            }
+            AnsiConsole.Markup("\n");
+        }
+        RerenderFlag = false;
     }
 }
